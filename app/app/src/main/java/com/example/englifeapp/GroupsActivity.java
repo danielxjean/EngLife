e71be14585b762f742aea80297f5d5c025e05049
Cedrik Dubois <cedrikdubois.edu@gmail.com>
2020-02-10 10:29:39 -0500

    First version of Groups page

--- app/app/src/main/java/com/example/englifeapp/GroupsActivity.java
+++ app/app/src/main/java/com/example/englifeapp/GroupsActivity.java
@@ -2,7 +2,13 @@ package com.example.englifeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class GroupsActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class GroupsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
+
+        configureInfoTextView();
     }
+
+    private void configureInfoTextView(){
+        TextView infoText = (TextView)findViewById(R.id.info_groups_textview);
+        String info = "On this page you can select from various Concordia societies.\n" +
+                      "Selecting a society will let you see their posts and updates \n" +
+                      "on your newsfeed. Societies post about events and information on how to become members!";
+        infoText.setText(info);
+    }
+
 }
--- app/app/src/main/java/com/example/englifeapp/MainActivity.java
+++ app/app/src/main/java/com/example/englifeapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.englifeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureSwitchToGroupsButton();
+    }
+
+    private void configureSwitchToGroupsButton(){
+        Button finishBtn = (Button)findViewById(R.id.finish_groups_button);
+
+        finishBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GroupsActivity.class));
+            }
+        });
     }
 }
