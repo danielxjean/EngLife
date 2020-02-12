be5672e00dfd9d1f5e2a8ba9f7384a1a612c5f9d
Amin Solki <aminn44@gmail.com>
2020-02-11 21:53:05 -0500

    Added 2 lines for test

--- app/app/src/main/java/com/example/englifeapp/MainActivity.java
+++ app/app/src/main/java/com/example/englifeapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.englifeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI) ;
+        startActivity(intent);
     }
 }
