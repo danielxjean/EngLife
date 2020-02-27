fd8aab2f7c61015a5c862030899f0c40fb4eb309
TonyMontana11 <anth.chraim@gmail.com>
2020-02-27 16:37:41 -0500

    added search page with hard-coded users

--- eng_life/lib/screens/home/home_screens/search.dart
+++ eng_life/lib/screens/home/home_screens/search.dart
@@ -1,6 +1,6 @@
 import 'package:eng_life/services/auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 class Search extends StatefulWidget {
   @override
   _SearchState createState() => _SearchState();
@@ -12,19 +12,101 @@ class _SearchState extends State<Search> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      child: Container(
-        color: Colors.grey[100],
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Text("Search users page in production")
-            ],
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Search User"),
+        backgroundColor: Colors.red[700],
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: () {
+            showSearch(context: context, delegate: DataSearch());
+          })
+        ],
       ),
+      drawer: Drawer(),
     );
   }
 }
+
+class DataSearch extends SearchDelegate<String>{
+
+final users = [
+  "test@m.com",
+  "a@m.com",
+  "test1@mail.com",
+  "admin@mail.com",
+];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    //actions for app bar
+    return [
+      IconButton(icon: Icon(Icons.clear), onPressed: () {
+        query = "";
+      })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    // leading icon on the left of the app bar
+    return IconButton(
+        icon: AnimatedIcon(
+            icon: AnimatedIcons.menu_arrow,
+            progress: transitionAnimation,
+        ),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    //show result based on selection
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        child: Card(
+          color: Colors.red,
+          child: Center(
+            child: Text(query),
+          ),
+        ),
+      )
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    // show when someone searches for something
+
+    final suggestionList = query.isEmpty ?
+                            users :
+                            users.where((p)=>p.startsWith(query)).toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        onTap: (){
+          showResults(context);
+        },
+        leading: Icon(Icons.person_add),
+        title: RichText(
+           text: TextSpan(
+             text: suggestionList[index].substring(0, query.length),
+             style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+             children: [TextSpan(
+               text: suggestionList[index].substring(query.length),
+               style: TextStyle(color: Colors.grey))
+             ]),
+           ),
+          ),
+      itemCount: suggestionList.length,
+    );
+  }
+}
+
+
